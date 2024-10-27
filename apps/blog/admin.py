64e6d872b023ad3f3d7a522356e117ea03ab1e67
Diane Corriette <dcorriette@gmail.com>
2024-10-27 15:34:40 +0000

    Add admin.py classes

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,74 @@
-from django.contrib import admin
+# apps/blog/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import Category, Tag, Post, Comment
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'post_count')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+    
+    def post_count(self, obj):
+        return obj.posts.count()
+    post_count.short_description = 'Number of Posts'
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'post_count')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+    
+    def post_count(self, obj):
+        return obj.posts.count()
+    post_count.short_description = 'Number of Posts'
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'status', 'is_featured', 
+                   'published_date', 'featured_image_preview')
+    list_filter = ('status', 'is_featured', 'category', 'tags', 'created_at')
+    search_fields = ('title', 'content', 'excerpt')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'published_date'
+    filter_horizontal = ('tags',)
+    readonly_fields = ('created_at', 'updated_at')
+    
+    fieldsets = (
+        ('Content', {
+            'fields': ('title', 'slug', 'content', 'excerpt', 'featured_image')
+        }),
+        ('Categorization', {
+            'fields': ('category', 'tags')
+        }),
+        ('Publishing', {
+            'fields': ('status', 'published_date', 'is_featured')
+        }),
+        ('SEO', {
+            'fields': ('meta_description',),
+            'classes': ('collapse',)
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    def featured_image_preview(self, obj):
+        if obj.featured_image:
+            return format_html('<img src="{}" style="max-height: 50px;"/>', 
+                             obj.featured_image.url)
+        return "No image"
+    featured_image_preview.short_description = 'Preview'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created_at', 'is_approved')
+    list_filter = ('is_approved', 'created_at')
+    search_fields = ('name', 'email', 'content', 'post__title')
+    actions = ['approve_comments']
+    
+    def approve_comments(self, request, queryset):
+        queryset.update(is_approved=True)
+    approve_comments.short_description = "Approve selected comments"
--- apps/portfolio/admin.py
+++ apps/portfolio/admin.py
@@ -1,3 +1,74 @@
-from django.contrib import admin
+# apps/portfolio/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import Technology, Project, ProjectImage
+
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+    fields = ('image', 'caption', 'order', 'image_preview')
+    readonly_fields = ('image_preview',)
+    
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 50px;"/>', 
+                             obj.image.url)
+        return "No image"
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'icon', 'project_count')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+    
+    def project_count(self, obj):
+        return obj.projects.count()
+    project_count.short_description = 'Number of Projects'
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'featured_image_preview', 'is_featured', 
+                   'order', 'created_at')
+    list_filter = ('is_featured', 'technologies', 'created_at')
+    search_fields = ('title', 'description', 'short_description')
+    prepopulated_fields = {'slug': ('title',)}
+    filter_horizontal = ('technologies',)
+    inlines = [ProjectImageInline]
+    list_editable = ('order', 'is_featured')
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'slug', 'short_description', 'description')
+        }),
+        ('Media', {
+            'fields': ('featured_image',)
+        }),
+        ('Technologies & Links', {
+            'fields': ('technologies', 'project_url', 'github_url')
+        }),
+        ('Display Options', {
+            'fields': ('is_featured', 'order')
+        }),
+    )
+    
+    def featured_image_preview(self, obj):
+        if obj.featured_image:
+            return format_html('<img src="{}" style="max-height: 50px;"/>', 
+                             obj.featured_image.url)
+        return "No image"
+    featured_image_preview.short_description = 'Preview'
+
+@admin.register(ProjectImage)
+class ProjectImageAdmin(admin.ModelAdmin):
+    list_display = ('project', 'image_preview', 'caption', 'order')
+    list_filter = ('project',)
+    search_fields = ('project__title', 'caption')
+    list_editable = ('order',)
+    
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 50px;"/>', 
+                             obj.image.url)
+        return "No image"
+    image_preview.short_description = 'Preview'
